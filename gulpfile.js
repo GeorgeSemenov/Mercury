7cc8cdb6a7980065d0efeef528c7a37e1f036798
GeorgeSemenov <zhora.sem@yandex.ru>
2018-09-28 15:30:01 +0300

    I already finished with 3 screens

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
+    haml = require('gulp-haml'),
     browserSync = require('browser-sync'),//Для обновления браузера при изменении
     concat = require('gulp-concat'),//Для объединения файлов js.
-    concatcss = require('gulp-concat-css'),//Для объединения файлов css.
     uglify = require('gulp-uglifyjs'),//Для сжатия кода.
     cssnano = require('gulp-cssnano'),//Для минификации css
     rename = require('gulp-rename'),//Для переименовывания файлов.
@@ -11,6 +11,7 @@ var gulp = require('gulp'),
     pngquant = require('imagemin-pngquant'),//Для работы с png
     giflossy = require('imagemin-giflossy'),
     mozjpeg = require('imagemin-mozjpeg'),
+    responsive = require('gulp-responsive-images'),
     cache = require('gulp-cache'),//Для кэширования изображений.
     autoprefixer = require('gulp-autoprefixer');//Эта штуковина будет добавлять автоматически вендорные приставки к свойствам css, вроде -webkit-flex, -moz-flex и т.п.
 
@@ -23,6 +24,12 @@ gulp.task('lessTask',function(){
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream: true}))
 });
+
+gulp.task('hamlTask',function(){
+    return gulp.src('app/*.haml')
+        .pipe(haml())
+        .pipe(gulp.dest('app/'))
+})
 
 gulp.task('brsyTask', function(){
    browserSync({
@@ -37,7 +44,7 @@ gulp.task('buildScripts', function(){
     return gulp.src([//Указываем объединяемые файлы
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/bootstrap/dist/js/bootstrap.min.js',
-        'node_modules/popper.js/dist/popper.min.js'
+        'node_modules/popper/dist/popper.min.js'
     ])
     .pipe(concat('libs.min.js'))//Объединяемые файлы будут собранны в
     .pipe(uglify())//Сжимаем файлик libs.min.js
@@ -48,7 +55,7 @@ gulp.task('buildCSS',['lessTask'],function(){
     return gulp.src([
         'node_modules/bootstrap/dist/css/bootstrap.min.css'
     ])
-    .pipe(concatcss('libs.min.css'))
+    .pipe(concat('libs.min.css'))
     .pipe(cssnano())
     //.pipe(rename({suffix:'.min'}))//Добавляем суффикс .min
     .pipe(gulp.dest('app/css'));
@@ -57,10 +64,67 @@ gulp.task('buildCSS',['lessTask'],function(){
 gulp.task('watcher',['brsyTask','lessTask','buildScripts','buildCSS'],function(){//Я убрал lessTask из списка первоначального исполнеиня т.к. из-за его первоначального срабатывания возникали проблемы.
     gulp.watch('app/less/**/*.less',['lessTask']);
     gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/*.haml',['hamlTask']);
     console.log("Wathcer is watch on you.");
 });
 
-gulp.task('convertImg',['clearCache'], function() {
+gulp.task('imgWatcher', function(){//ещё один watcher чтобы обрабатывать адаптивные изображения, без него не получается.
+    var watchaga = gulp.watch('app/img/**/*', ['convertImg']);
+    setTimeout(watchaga.end,30*1000);//Смотрящий выключится через 30 секунд и не будет наблюдать.
+})
+
+//Чтобы обработать изображения - запусти buildImg
+gulp.task('buildImg', function() {
+    gulp.src('app/img/**/*')
+    .pipe(responsive({
+        '**/*.*': [{
+            width: 2240,
+            suffix: '-2240',
+            quality: 70
+        }, {
+            width: 1920,
+            suffix: '-1920',
+            quality: 70
+        }, {
+            width: 1600,
+            suffix: '-1600',
+            quality: 70
+        }, {
+            width: 1280,
+            suffix: '-1280',
+            quality: 70
+        }, {
+            width: 1280,
+            height: 300,
+            crop: true,
+            gravity: 'Center',
+            suffix: '-thumbwide',
+            quality: 70
+        }, {
+            width: 960,
+            suffix: '-960',
+            quality: 70
+        }, {
+            width: 640,
+            suffix: '-640',
+            quality: 70
+        }, {
+            width: 480,
+            suffix: '-480',
+            quality: 70
+        }, {
+            width: 320,
+            suffix: '-320',
+            quality: 70
+        }]
+    }))
+    .pipe(gulp.dest(function(file) {
+        var str = file.base.replace("app/img/originalImg","app/img/")
+        return str;
+    }));
+});
+
+gulp.task('convertImg', function() {
     return gulp.src(['app/img/**/*'])
         .pipe(cache(imagemin([
             //png
@@ -88,7 +152,11 @@ gulp.task('convertImg',['clearCache'], function() {
                 quality: 85//Этот параметр нужен для того, чтобы увеличивить сжимаемость jpg файлов, особой потери качества я не заметил.
             })
         ])))
-        .pipe(gulp.dest('dist/img'));
+//        .pipe(rename({suffix:'-converted'})) //Учти, что суффикс будет добавляться и подпапкамы
+        .pipe(gulp.dest(function(file){
+            console.log('file = ' + file.name);
+            return file.base;
+    }))
 });
 
 gulp.task('clearCache', function(){
@@ -99,23 +167,23 @@ gulp.task('clean',function(){
     return del.sync('dist');//Удаляем все файлы в папке dist
 })
 
-gulp.task('build',['clean','convertImg','lessTask','buildScripts'],function(){
+gulp.task('build',['clean','lessTask','buildScripts','hamlTask'],function(){
     var distCSS = gulp.src([ //Назначая переменным действия мы одновременно их выплоняем.
         'app/css/style.css',
         'app/css/libs.min.css'
     ])
     .pipe(gulp.dest('dist/css'))
-
+    
     var distFonts = gulp.src([
         'app/fonts/**/*'
     ])
     .pipe(gulp.dest('dist/fonts'))
-
+    
     var distJS = gulp.src([
         'app/js/**/*'
     ])
     .pipe(gulp.dest('dist/js'))
-
+    
     var distHtml = gulp.src('app/*.html')
     .pipe(gulp.dest('dist'));
 });
